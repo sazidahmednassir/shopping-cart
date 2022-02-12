262b5e021cd9e8a5b250012d7cd56cf92df9d357
sazidahmednassir <sazidahmednassir@gmail.com>
2022-02-12 11:24:39 -0800

    first event added

--- app.js
+++ app.js
@@ -1,6 +1,34 @@
+ function updateProductNumber(product, price,  isIncreasing){
+    const productInput=document.getElementById( product + '-number');
+    let productNumber=productInput.value;
+    if(isIncreasing){
+        productNumber=parseInt(productNumber)+ 1;
+    }
+    else if(productNumber > 0){
+        productNumber=parseInt(productNumber) - 1;
+    }
+    productInput.value=productNumber;
+    //UPDATE CASE TOTAL
+    const producttotal=document.getElementById(product +'-total');
+    producttotal.innerText=productNumber*price;
+}
+
+document.getElementById('phone-plus').addEventListener('click', function(){
+    updateProductNumber('phone', 1219, true)
+})
+
+document.getElementById('phone-minus').addEventListener('click', function(){
+    updateProductNumber('phone', 1219)
+})
+
 document.getElementById('case-plus').addEventListener('click', function(){
-    const caseInput=document.getElementById('case-number');
-    const caseNumber=caseInput.value;
-    caseInput.value=parseInt(caseNumber) + 1;
+    updateProductNumber('case', 59, true)
+}
+)
+document.getElementById('case-minus').addEventListener('click', function(){
+
+    
+    updateProductNumber('case', 59)
+    
     
 })
