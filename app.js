5137dd1e2b4b931533c2719a68ecfe102e4d61f7
sazidahmednassir <sazidahmednassir@gmail.com>
2022-02-12 11:49:35 -0800

    cal all

--- app.js
+++ app.js
@@ -16,7 +16,7 @@
     calculateTotal();
 }
 
-function getInputValue(){
+function getInputValue(product){
     const productInput=document.getElementById(product + '-number');
     const productNumber= parseInt( productInput.value);
     return productNumber;
@@ -28,6 +28,12 @@ function calculateTotal(){
     const phoneTotal=  getInputValue('phone') *1219;
     const caseTotal=  getInputValue('case') *59;
     const subTotal= phoneTotal+caseTotal;
+    const tax= subTotal /10;
+    const totalPrice= subTotal + tax;
+
+    document.getElementById('sub-total').innerText=subTotal;
+    document.getElementById('tax-amount').innerText=tax;
+    document.getElementById('total-price').innerText=totalPrice;
 
 }
 
